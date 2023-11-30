41844c6fd510d8f1fbeb571c8525e299e3ec2c7e
Eden.Fang <eden.fang@wh8office.com>
2023-11-30 18:24:22 +0800

    feat: 新增資料過濾、排序、頁

--- Repositories/SQLWalkRepository.cs
+++ Repositories/SQLWalkRepository.cs
@@ -14,9 +14,37 @@ public class SQLWalkRepository : IWalkRepository
         this.dbContext = dbContext;
     }
 
-    public async Task<List<Walk>> GetAllAsync()
+    public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null,
+        string? sortBy = null,
+        bool isAscending = true, int pageNumber = 1, int pageSize = 1)
     {
-        return await dbContext.Walks.Include("Difficulty").Include("Region").ToListAsync();
+        // return await dbContext.Walks.Include("Difficulty").Include("Region").ToListAsync();
+        var walks = dbContext.Walks.Include("Difficulty").Include("Region").AsQueryable();
+
+        if (string.IsNullOrWhiteSpace(filterOn) == false && string.IsNullOrWhiteSpace(filterQuery) == false)
+        {
+            if (filterOn.Equals("Name", StringComparison.OrdinalIgnoreCase))
+            {
+                walks = walks.Where(x => x.Name.Contains(filterQuery));
+            }
+        }
+
+        if (string.IsNullOrWhiteSpace(sortBy) == false)
+        {
+            if (sortBy.Equals("Name", StringComparison.OrdinalIgnoreCase))
+            {
+                walks = isAscending ? walks.OrderBy(x => x.Name) : walks.OrderByDescending(x => x.Name);
+            }
+            else if (sortBy.Equals("Length", StringComparison.OrdinalIgnoreCase))
+            {
+                walks = isAscending ? walks.OrderBy(x => x.LengthInKm) : walks.OrderByDescending(x => x.LengthInKm);
+            }
+        }
+
+        var skipResult = (pageNumber - 1) * pageSize;
+        
+        return await walks.Skip(skipResult).Take(pageSize).ToListAsync();
+        // return await walks.ToListAsync();
     }
 
     public async Task<Walk> GetByIdAsync(Guid id)
@@ -27,14 +55,14 @@ public class SQLWalkRepository : IWalkRepository
             .FirstOrDefaultAsync(w => w.Id == id);
     }
 
-    
+
     public async Task<Walk> CreateAsync(Walk walk)
     {
         await dbContext.Walks.AddAsync(walk);
         await dbContext.SaveChangesAsync();
         return walk;
     }
-    
+
     public async Task<Walk?> UpdateAsync(Guid id, Walk walk)
     {
         var existingWalk = await dbContext.Walks.FindAsync(id);
